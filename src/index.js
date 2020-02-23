36741de4328f4ae2f6550e9ae0f41da3e343e4f9
Moskvichev Vladislav <moskvichev.exceedteam@gmail.com>
2020-02-23 18:36:12 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from "react-router-dom";
 import {store} from "./Components/ReduxCounter";
 
-let rerender = () => {
+const rerender = () => {
     ReactDOM.render(
         <BrowserRouter>
             <App/>
